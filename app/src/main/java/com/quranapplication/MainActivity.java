5c334abbce3c91bf11672bb7ab60367f89f70269
Ansha Sarwar <anshasarwar6@gmail.com>
2023-06-04 21:30:20 +0500

    First

---
+++ app/src/main/java/com/quranapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.quranapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.quranapplication.QuranData;
+import com.quranapplication.ParahSurah;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
